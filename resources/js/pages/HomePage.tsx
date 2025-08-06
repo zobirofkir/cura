49f5d224cdc99d820ae1437f918da0c3e46f1e0a
csw <zobirofkir19@gmail.com>
2025-08-06 14:30:21 +0100

    use this images in home screen

--- resources/js/pages/HomePage.tsx
+++ resources/js/pages/HomePage.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import ImageOne from '@/images/image-1.jpeg';
+import ImageTwo from '@/images/image-2.jpeg';
+import ImageThree from '@/images/image-3.jpeg';
+import ImageFour from '@/images/image-4.jpeg';
+import ImageFive from '@/images/image-5.jpeg';
+import ImageSex from '@/images/image-6.jpeg';
+import ImageSeven from '@/images/image-7.jpeg';
+import ImageEight from '@/images/image-8.jpeg';
 
 const HomePage = () => {
   return (
