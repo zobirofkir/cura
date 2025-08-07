ad5a4f7ea38823a8ed66545ba9e45e0169e3157d
csw <zobirofkir19@gmail.com>
2025-08-07 21:19:47 +0100

    add max weight

--- resources/js/components/PackComponent.tsx
+++ resources/js/components/PackComponent.tsx
@@ -27,7 +27,7 @@ const PackComponent: React.FC<PackProps> = ({
 }) => {
   return (
     <div className="py-8 px-4">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
